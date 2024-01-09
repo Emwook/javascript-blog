e186672ba029491842990f55f61d3a729ccf6f02
Emilia Łukasiuk <lukasiuk.emilia@gmail.com>
2024-01-09 13:17:00 +0100

    add major resizing setting changes

--- js/script.js
+++ js/script.js
@@ -4,8 +4,6 @@ function titleClickHandler(event){
   event.preventDefault();
   const clickedElement = this;
 
-  console.log('Link was clicked!');
-
   /* [DONE] remove class 'active' from all article links  */
 
   const activeLinks = document.querySelectorAll('.titles a.active');
@@ -32,8 +30,41 @@ function titleClickHandler(event){
   let targetArticle = document.querySelector(articleSelector);
 
   /* [DONE] add class 'active' to the correct article */
-  targetArticle.classList.add('active');   
+  targetArticle.classList.add('active');
+}   
+
+function resizePostWindow(){
+  /* get sidebar left section selector */
+  let sidebarLeft = document.querySelector('.left');
+
+  /* read sidebar left height*/
+  let sidebarLeftHeight = sidebarLeft.clientHeight;
+
+
+  /* select an article with class active*/
+  let activeArticle = document.querySelector('.posts article.active');
+
+  /* get active article height and add 15px as a bottom margin*/
+  let activeArticleHeight = activeArticle.clientHeight;
+  activeArticleHeight = activeArticleHeight + 15;
+
+  /* make a selector for the posts section */
+  let postsSectionSelector = document.querySelector('.posts');
+  console.log(sidebarLeftHeight);
+
+  /* [BUGGED] set active article height to sidebar height if the center section in layout type 1-1-1 is shorter than sidebars*/
+  /* BUGGED: not reseting back to a shorter window height when switched to a taller window by an article setting; dead space leftover */
+  if (activeArticleHeight < sidebarLeftHeight && window.innerWidth > 932 ){
+    activeArticleHeight = sidebarLeftHeight + 2;
+  }
+
+  /* set posts section height to equal active article height */
+  let postsSectionHeight = postsSectionSelector.style.height = activeArticleHeight + 'px';
+  
+  console.log(postsSectionHeight);
 }
+
+resizePostWindow();
 
 const optArticleSelector = '.post',
   optTitleSelector = '.post-title',
@@ -70,11 +101,12 @@ function generateTitleLinks(){
   titleList.innerHTML = html;
 
   const links = document.querySelectorAll('.titles a');
-  console.log(links);
 
   for(let link of links){
     link.addEventListener('click', titleClickHandler);
+    link.addEventListener('click', resizePostWindow);
   }
+  window.addEventListener('resize',resizePostWindow);
 }
 
 generateTitleLinks();
